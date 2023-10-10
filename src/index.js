614e07e59f74b5b65e310a9677f3715b371ce71d
nagy.gabor <nagy.gabor@dss.hu>
2023-10-10 13:38:23 +0200

    modify supplier path

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
                 element: <Products />,
             },
             {
-                path: "supplierorders",
+                path: "supplierordersum",
                 element: <Suppliers />,
             },
             {
